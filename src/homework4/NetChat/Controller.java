f2fa20ad8e8e2f95f94e56e0ac8ea72e58aac2e7
DavidSwan <ljvf@mail.ru>
2020-02-09 14:21:04 +0300

    Сделано ДЗ №4 - окно сетевого чата  - Работа с оформлением

--- src/homework4/NetChat/Controller.java
+++ src/homework4/NetChat/Controller.java
@@ -1,13 +1,17 @@
 package homework4.NetChat;
 
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public class Controller<event> {
 
@@ -18,12 +22,14 @@ public class Controller<event> {
     @FXML
     private TextField text;
 
+
     public void SendMessage(ActionEvent actionEvent) {
         if (text.getText().isEmpty()) {
             text.requestFocus();
-
         } else
-            textArea.appendText("User: " + text.getText() + "\n");
+            textArea.appendText("User "
+                    + new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime())
+                    + " : " + text.getText() + "\n");
         text.clear();
         text.requestFocus();
     }
@@ -33,10 +39,11 @@ public class Controller<event> {
             text.requestFocus();
         } else {
             if (keyEvent.getCode() == KeyCode.ENTER) {
-                textArea.appendText("User: " + text.getText() + "\n");
+                textArea.appendText("User "
+                        + new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime())
+                        + " : " + text.getText() + "\n");
                 text.clear();
                 text.requestFocus();
-
             }
         }
     }
--- src/homework4/NetChat/Main.java
+++ src/homework4/NetChat/Main.java
@@ -4,18 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("DS Chat");
+        primaryStage.getIcons().add(new Image("/homework4/NetChat/image/icon.png"));
+        primaryStage.setTitle("DS Chat - " + new SimpleDateFormat("MM.dd.yyyy").format(Calendar.getInstance().getTime()));
         primaryStage.setScene(new Scene(root, 400, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
